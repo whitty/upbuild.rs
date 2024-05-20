0a04fc4f6e5b32031d26f207cd36ca19c831baa0
Greg Whiteley <greg.whiteley@gmail.com>
2024-05-20 22:53:54 +1000

    lib: re-export symbols

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
-pub mod error;
-pub mod file;
-pub mod exec;
-pub mod find;
-pub mod args;
+mod error;
+mod file;
+mod exec;
+mod find;
+mod args;
+
+pub use file::ClassicFile;
+
+pub use exec::Exec;
+pub use exec::process_runner;
+pub use exec::print_runner;
+
+pub use find::find;
+pub use args::Config;
 
 pub type Error = error::Error;
 pub type Result<T> = std::result::Result<T, error::Error>;
