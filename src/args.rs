811fa61e1859bc281f69e987c62273d9d4ef0054
Greg Whiteley <greg.whiteley@gmail.com>
2024-05-20 18:13:09 +1000

    add args

---
+++ src/args.rs
@@ -0,0 +1,178 @@
+use std::collections::HashSet;
+
+#[derive(Debug, Default, PartialEq)]
+pub struct Config {
+    print: bool,
+    select: HashSet<String>,
+    reject: HashSet<String>,
+}
+
+fn apply_tags(arg: &str, add: &mut HashSet<String> , drop: &mut HashSet<String>) -> bool {
+    match arg.split_once('=') {
+        Some((_, arg)) => {
+            if !arg.is_empty() {
+                add.insert(arg.to_string());
+                drop.remove(arg);
+                return true;
+            }
+        },
+        None => return false,
+    }
+    false
+}
+
+impl Config {
+    pub fn parse<T>(args: T) -> (std::iter::Peekable<T>, Config)
+    where
+        T: Iterator<Item=String>
+    {
+        let mut args = args.peekable();
+        let mut cfg = Config { ..Default::default() };
+
+        loop {
+            if let Some(arg) = args.peek() {
+                if arg.starts_with("--") {
+                    match &arg[2..] {
+                        "ub-print" => {
+                            cfg.print = true;
+                        },
+                        "-" => todo!("--- not handled yet"),
+                        "" => { args.next(); break; },
+                        _ => {
+                            if arg.starts_with("--ub-select=") {
+                                if ! apply_tags(arg, &mut cfg.select, &mut cfg.reject) {
+                                    break;
+                                }
+                            } else if arg.starts_with("--ub-reject=") {
+                                if ! apply_tags(arg, &mut cfg.reject, &mut cfg.select) {
+                                    break;
+                                }
+                            } else {
+                                break;
+                            }
+                        },
+                    };
+
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+            args.next();
+        }
+        (args, cfg)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn args<const N: usize>(args: [&str; N]) -> std::vec::IntoIter<String> {
+        let v: Vec<String> = args.into_iter().map(|x| x.to_string()).collect();
+        v.into_iter()
+    }
+
+    fn do_parse<const N: usize>(a: [&str; N]) -> (Vec<String>, Config) {
+        let (v, args) = Config::parse(args(a));
+        (v.collect(), args)
+        //parse(args(a)).collect()
+    }
+
+    #[test]
+    fn test_parse() {
+        let (v, args) = do_parse([]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config::default());
+
+        let (v, args) = do_parse(["a", "b"]);
+        assert_eq!(v, ["a", "b"]);
+        assert_eq!(args, Config::default());
+
+        let (v, args) = do_parse(["--"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config::default());
+
+        let (v, args) = do_parse(["--ub-print"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config { print: true, ..Config::default() });
+
+        let (v, args) = do_parse(["--ub-print", "a", "b"]);
+        assert_eq!(v, ["a", "b"]);
+        assert_eq!(args, Config { print: true, ..Config::default() });
+
+        // after any non-matched arguments we'accept normal arguments
+        let (v, args) = do_parse(["a", "b", "--ub-print"]);
+        assert_eq!(v, ["a", "b", "--ub-print"]);
+        assert_eq!(args, Config { print: false, ..Config::default() });
+
+        // check -- to end parsing
+        let (v, args) = do_parse(["--", "--ub-print"]);
+        assert_eq!(v, ["--ub-print"]);
+        assert_eq!(args, Config { print: false, ..Config::default() });
+
+        // check -- to end parsing
+        let (v, args) = do_parse(["--"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config { ..Config::default() });
+    }
+
+    fn string_set<const N: usize>(list: [&str; N]) -> HashSet<String> {
+        HashSet::from(list.map(|s| s.to_string()))
+    }
+
+    #[test]
+    fn test_parse_tags() {
+        let (v, args) = do_parse(["--ub-select=foo"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config { select: string_set(["foo"]), ..Config::default() });
+
+        let (v, args) = do_parse(["--ub-reject=foo"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config { reject: string_set(["foo"]), ..Config::default() });
+
+        let (v, args) = do_parse(["--ub-reject=foo", "--ub-select=bar"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config {
+            select: string_set(["bar"]),
+            reject: string_set(["foo"]),
+            ..Config::default()
+        });
+
+        let (v, args) = do_parse(["--ub-reject=foo", "--ub-select=bar", "--ub-select=foo"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config {
+            select: string_set(["bar", "foo"]),
+            ..Config::default()
+        });
+
+        let (v, args) = do_parse(["--ub-reject=foo", "--ub-select=bar", "--ub-reject=bar"]);
+        assert!(v.is_empty(), "!is_empty: was {:?}", v);
+        assert_eq!(args, Config {
+            reject: string_set(["bar", "foo"]),
+            ..Config::default()
+        });
+
+        let (v, args) = do_parse(["--ub-reject=foo", "--ub-select=bar", "--", "--ub-reject=bar"]);
+        assert_eq!(v, ["--ub-reject=bar"]);
+        assert_eq!(args, Config {
+            select: string_set(["bar"]),
+            reject: string_set(["foo"]),
+            ..Config::default()
+        });
+
+        let (v, args) = do_parse(["--ub-reject"]);
+        assert_eq!(v, ["--ub-reject"]);
+        assert_eq!(args, Config { ..Config::default() });
+
+        let (v, args) = do_parse(["--ub-select"]);
+        assert_eq!(v, ["--ub-select"]);
+        assert_eq!(args, Config { ..Config::default() });
+
+        let (v, args) = do_parse(["--ub-select="]);
+        assert_eq!(v, ["--ub-select="]);
+        assert_eq!(args, Config { ..Config::default() });
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod error;
 pub mod file;
 pub mod exec;
 pub mod find;
+pub mod args;
 
 pub type Error = error::Error;
 pub type Result<T> = std::result::Result<T, error::Error>;
