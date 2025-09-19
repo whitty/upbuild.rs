e11454ca79afb127841a657b3cb007745f25f79f
Greg Whiteley <greg.whiteley@gmail.com>
2025-09-20 01:07:41 +1000

    first cut of bash_completion.sh

---
+++ etc/bash_completion.sh
@@ -0,0 +1,38 @@
+# bash completion for upbuild                              -*- shell-script -*-
+
+_upbuild()
+{
+  IFS=$'\n'
+  local cur prev words cword split # needed by _init_completion()
+  # Do not treat = as word breaks even if they are in $COMP_WORDBREAKS:
+  # Split option=value into option in $prev and value in $cur
+  _init_completion -s || return
+
+  case $prev in
+    '--ub-select'|'--ub-reject')
+      # TODO generate this
+      OPTS=(fresh arm)
+      COMPREPLY=( $(compgen -W "${OPTS[*]}" -- $cur) )
+      return 0
+      ;;
+  esac
+
+  case $cur in
+    -*)
+      # TODO generate this
+      OPTS=(--ub-print --ub-add --ub-no-env --ub-select= --ub-reject=)
+      # suppress the space
+      compopt -o nospace
+      # add the space back in where needed
+      for i in ${!OPTS[*]}; do [[ ${OPTS[$i]} == *= ]] || OPTS[$i]+=' ' ; done
+      COMPREPLY=( $(compgen -W "${OPTS[*]}" -- "$cur") )
+      return 0
+      ;;
+  esac
+  compopt -o bashdefault -o default
+  # completing with "" should go to bashdefault (normal completion)
+  # could try to get fancy I guess?
+  COMPREPLY=( $(compgen -W "" -- $cur) )
+  return 0
+}
+complete -F _upbuild upbuild
